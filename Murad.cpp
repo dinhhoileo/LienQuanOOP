930218cfe8e159ff7e0410bf7821ef5f44818f4a
dinhhoileo <dinhhoi2004@gmail.com>
2023-05-02 13:20:07 +0700

    update

--- Murad.cpp
+++ Murad.cpp
@@ -13,5 +13,49 @@ public:
         this->typeHero = typeHero;
     }
 
+    std::string getTypeHero(){return this->typeHero;}
+    void getTypeHero(std::string typeHero){this->typeHero=typeHero;}
+
+    void setInformation() 
+    {
+        Hero::setInfo();
+        std::cout<<"Type Hero: ";
+        std::cin>>this->typeHero;
+    }
+
+     void getInformation() 
+    {
+        std::cout<<"Murad"<<std::endl;
+        std::cout<<"Type Hero:"<<this->typeHero<<std::endl;
+        Hero::getInfor();
+    }
     
+    void attack() override
+    {
+        skill_1();
+        skill_2();
+        ultimate();
+    }
+
+    void skill_1()
+    {
+        std::cout<<"Luot 3 lan:"<<std::endl;
+    }
+    void skill_2()
+    {
+        std::cout<<"Xoay ao anh"<<std::endl;
+    }
+    void ultimate()
+    {
+        std::cout<<"Ao anh tram:"<<std::endl;
+    }
+    int move(int setSpeed()) override
+    {
+        return getSpeed();
+    }
+
+    void defend() override
+    {
+        std::cout<<"FULL TANK"<<std::endl;
+    }
 };
